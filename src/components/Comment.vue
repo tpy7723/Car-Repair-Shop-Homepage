7e06cfadc8f90575dcb6cc46ba2913df4247e36c
tpy7723 <tpy7723@naver.com>
2018-12-17 00:47:48 +0900

    댓글현황

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="comment">
-  댓글 현황
+  댓글 달리지 않은 게시글
   <div class="board-box">
     <div class="row form-group">
       <div class="col-sm-10"></div>
@@ -36,19 +36,61 @@
 </template>
 
 <script>
-export default {
-  name: 'comment',
-  data () {
-    return {
-      LoginID: '',
-      Password: '',
-      Name: '',
-      Phone: ''
+  export default {
+    name: "question",
+    data() {
+      return {
+        msg: '게시판',
+        list: [],
+        time: 0,
+        duration: 5000
+      }
+    },
+    computed: {
+      isLogged () {
+        console.log(this.$store.getters.isLogged)
+        return this.$store.getters.isLogged
+      },
+      user_id() {
+        return this.$store.getters.getId
+      }
+    },
+    mounted: function() {
+      this.msg = ''
+      console.log('질문게시판')
+      this.getData()
+    },
+    methods: {
+      Add_comment: function(item) {
+        this.$router.push({
+          name: 'Comment_add',
+          query: {
+            num: item.질문번호
+          }
+        })
+      },
+      getData: function() {
+        var url = 'http://106.10.32.228:3000/call/notcommentedquestion'
+        console.log(url)
+        this.$http.get(url)
+          .then(result => {
+            console.log(result)
+            console.log(result.data.status)
+            this.list = result.data.result
+            console.log(this.list)
+            this.list.forEach(v => {
+              var datainfo = v.작성시간
+              v.작성시간 = this.$moment(dateinfo).tz('Asia/Seoul').format('YYYY년 M월 D일 H시 m분')
+            })
+          })
+          .catch(error => {
+            console.log('서버에러')
+            console.log(error)
+          })
+      },
     }
-  },
-}
-</script>
-
+  }
+  </script>
 
 
 <style media="screen">
