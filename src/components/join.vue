5a8c9f3b6425dc7693303491977b8c1cb6707d63
tpy7723 <tpy7723@naver.com>
2018-12-17 23:25:22 +0900

    정규식

--- src/components/join.vue
+++ src/components/join.vue
@@ -1,33 +1,37 @@
 <template>
-  <div class="join">
+  <div class="login">
     <br><br>
     <h1 italic> 고객 회원가입 </h1>
     <br><br>
 
     <form name="registerForm" action="register_ok.asp" method="post">
-  <div class="form-row">
-    <div class="form-group col-md-6">
+      <div class="login">
+    <div class="login">
       <label for="LoginID">ID</label>
       <input type="text" class="form-control" id='LoginID' v-model="LoginID" placeholder="LoginID">
     </div>
-    <div class="form-group col-md-6">
+    <div class="login">
       <label for="Password">Password</label>
       <input type="password" class="form-control" id='password' v-model="Password" placeholder="Password">
     </div>
-  </div>
-  <div class="form-row">
-  <div class="form-group col-md-6">
+    <div class="login">
+      <label for="Password">Re-Password</label>
+      <input type="password" class="form-control" id='pw_ok' placeholder="Re-Password">
+    </div>
+  <div class="login">
     <label for="Name">Name</label>
     <input type="text" class="form-control" id='name' v-model="Name" placeholder="홍길동">
   </div>
-  <div class="form-group col-md-6">
+  <div class="login">
     <label for="Phone">Phone</label>
     <input type="text" class="form-control" id='phone' v-model="Phone" placeholder="010-3017-2440">
   </div>
   <br>
-</div>
+
 <br>
+  <button type="submit" class="btn btn-primary" @click.prevent="goBack">       뒤로가기       </button>
   <button type="submit" class="btn btn-primary" @click.prevent="submit">       제출       </button>
+  </div>
 </form>
   </div>
 </template>
@@ -50,6 +54,9 @@ export default {
     }
   },
   methods: {
+    goBack: function() {
+      this.$router.push("/")
+    },
       logIn(data){
           console.log(data)
           this.$store.commit('logIn', {
@@ -61,12 +68,15 @@ export default {
         var regex;
         var LoginID = registerForm.LoginID.value;
         var password= registerForm.password.value;
+        var pw_ok = registerForm.pw_ok.value;
         var name = registerForm.name.value;
         var phone= registerForm.phone.value;
         if(LoginID==""){alert("ID를 입력해주세요"); return;}
         if(password==""){alert("Password를 입력해주세요"); return;}
+        if(pw_ok==""){alert("비밀번호 확인을 입력해주세요"); return;}
         if(name==""){alert("Name을 입력해주세요"); return;}
         if(phone==""){alert("Phone을 입력해주세요"); return;}
+        if(password!=pw_ok){alert("비밀번호와 비밀번호 확인이 다릅니다"); return;}
         regex=/^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$/;
         if(regex.test(phone)===false) {
           alert("잘못된 전화번호입니다.");
@@ -80,6 +90,7 @@ export default {
               if(result.data.status == 'success'){ // 로그인 성공
                   console.log('success')
                   this.$router.push("/")
+                  alert("성공적으로 가입되었습니다! 로그인 해주세요~");
               }
               else {
                 console.log('error')
@@ -108,10 +119,9 @@ export default {
 
 <style media="screen">
 .login{
-  width: 25%;
+  width: 60%;
   padding-top: 10px;
-  margin-left: auto;
-  margin-right: auto;
   text-align : center;
+
 }
 </style>
--- src/components/join_em.vue
+++ src/components/join_em.vue
@@ -1,43 +1,42 @@
 <template>
-<div class="join_em">
+<div class="login">
   <br><br>
   <h1 italic> 직원 회원가입 </h1>
   <br><br>
 
-  <form>
-    <div class="form-row">
-      <div class="form-group col-md-6">
+  <form name="registerForm">
+      <div class="login">
         <label for="LoginID">Employ Number</label>
-        <input type="text" class="form-control" v-model="LoginID" placeholder="123456">
+        <input type="text" class="form-control" id='id' v-model="LoginID" placeholder="123456">
       </div>
-      <div class="form-group col-md-6">
+      <div class="login">
         <label for="Password">Password</label>
-        <input type="password" class="form-control" v-model="Password" placeholder="Password">
+        <input type="password" class="form-control" id='password' v-model="Password" placeholder="Password">
       </div>
-    </div>
-    <div class="form-row">
-      <div class="form-group col-md-6">
+      <div class="login">
+        <label for="Password">Re-Password</label>
+        <input type="password" class="form-control" id='pw_ok' placeholder="Re-Password">
+      </div>
+      <div class="login">
         <label for="Name">Name</label>
-        <input type="text" class="form-control" v-model="Name" placeholder="홍길동">
+        <input type="text" class="form-control" id='name' v-model="Name" placeholder="홍길동">
       </div>
-      <div class="form-group col-md-6">
+      <div class="login">
         <label for="Phone">Phone</label>
-        <input type="text" class="form-control" v-model="Phone" placeholder="010-3017-2440">
+        <input type="text" class="form-control" id='phone' v-model="Phone" placeholder="010-3017-2440">
       </div>
-      <br>
-    </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
+    <div class="login">
       <label for="Start_date">Start Working Date</label>
-      <input type="text" class="form-control" v-model="Start_date" placeholder="2018-12-01">
+      <input type="text" class="form-control" id='start' v-model="Start_date" placeholder="2018-12-01">
     </div>
-    <div class="form-group col-md-6">
+    <div class="login">
       <label for="Supervisor">Supervisor Num</label>
-      <input type="text" class="form-control" v-model="Supervisor" placeholder="123456 or NULL">
+      <input type="text" class="form-control" id='super_' v-model="Supervisor" placeholder="123456 or NULL">
     </div>
     <br>
-  </div>
+
 <br>
+<button type="submit" class="btn btn-primary" @click.prevent="goBack">       뒤로가기       </button>
 <button type="submit" class="btn btn-primary" @click.prevent="submit"> 제출 </button>
 </form>
 </div>
@@ -63,6 +62,9 @@ export default {
     }
   },
   methods: {
+    goBack: function() {
+      this.$router.push("/")
+    },
     logIn(data) {
       console.log(data)
       this.$store.commit('logIn', {
@@ -71,6 +73,28 @@ export default {
       })
     },
     submit: function() {
+      var regex;
+      var id = registerForm.id.value;
+      var password= registerForm.password.value;
+      var pw_ok = registerForm.pw_ok.value;
+      var name = registerForm.name.value;
+      var phone= registerForm.phone.value;
+      var start= registerForm.start.value;
+      var super_= registerForm.super_.value;
+      if(id==""){alert("ID를 입력해주세요"); return;}
+      if(password==""){alert("Password를 입력해주세요"); return;}
+      if(pw_ok==""){alert("비밀번호 확인을 입력해주세요"); return;}
+      if(name==""){alert("Name을 입력해주세요"); return;}
+      if(phone==""){alert("Phone을 입력해주세요"); return;}
+      if(start==""){alert("Start Working Date를 입력해주세요"); return;}
+      if(super_=="NULL"){this.Supervisor=''}
+      if(password!=pw_ok){alert("비밀번호와 비밀번호 확인이 다릅니다"); return;}
+      regex=/^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$/;
+      if(regex.test(phone)===false) {
+        alert("잘못된 전화번호입니다.");
+        return;
+      }
+
       var url = 'http://106.10.32.228:3000' + `/request/join_em?ID=${this.LoginID}&PHONE=${this.Phone}&NAME=${this.Name}&START=${this.Start_date}&SUPER=${this.Supervisor}&PASSWORD=${this.Password}`;
       console.log(url)
       this.$http.get(url)
@@ -79,6 +103,7 @@ export default {
         .then((result) => {
           if (result.data.status == 'success') { // 로그인 성공
             console.log('success')
+            alert("성공적으로 가입되었습니다! 로그인 해주세요~");
             this.$router.push("/")
           }else {
             console.log('error')
@@ -107,10 +132,9 @@ export default {
 
 <style media="screen">
 .login {
-  width: 25%;
+  width: 60%;
   padding-top: 10px;
-  margin-left: auto;
-  margin-right: auto;
-  text-align: center;
+  text-align : center;
+
 }
 </style>
