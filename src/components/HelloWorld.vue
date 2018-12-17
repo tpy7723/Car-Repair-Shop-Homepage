61f5ecc3238e8537585eb8b7b9d49a65e7f98f15
tpy7723 <tpy7723@naver.com>
2018-12-18 03:27:08 +0900

    할인율

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,8 +6,10 @@
         <br>
         <h3 style='color : red' v-show="isLogged">{{this.ID}}</h3>
         <br>
-        <h3 v-show="isLogged">안녕하세요~</h3>
-
+        <h3 v-show="isLogged && this.em == 0">{{this.list[0].이름}}님 안녕하세요~</h3>
+        <br>
+        <h3 v-show="isLogged && this.em == 0">이용횟수: {{this.list[0].서비스이용횟수}} 이므로</h3>
+        <h3 v-show="isLogged && this.em == 0">할인율: {{this.discount}}% 입니다! </h3>
       </div>
     </body>
   </div>
@@ -20,14 +22,42 @@
     data() {
       return {
         Log: this.$store.getters.isLogged,
-        ID: this.$store.getters.getId
+        ID: this.$store.getters.getId,
+        discount:'0',
+        list:[0],
+        em:0,
       }
     },
     mounted: function() {
-      this.msg = ''
-      console.log('질문게시판')
       this.getData()
+      console.log(this.ID)
     },
+    methods: {
+      getData: function() {
+        var url = 'http://106.10.32.228:3000/hello'+`?ID=${this.$store.getters.getId}`
+        console.log(url)
+        this.$http.get(url)
+          .then(result => {
+            if(result.data.status == 'success'){ // 로그인 성공
+              console.log(result)
+              console.log(result.data.status)
+              this.list = result.data.result
+              if (this.list[0].서비스이용횟수>=5){
+                this.discount=5;
+              }
+            }else if(result.data.status == '직원'){
+              console.log('직원')
+              console.log(this.isLogged)
+              this.em=1;
+            }
+          })
+          .catch(error => {
+            console.log('서버에러')
+            console.log(error)
+          })
+      }
+    },
+
     computed: {
       isLogged () {
         console.log(this.$store.getters.isLogged)
